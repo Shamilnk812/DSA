a5eec8cc59be760d831f5f6e70b34dfb7ecfea89
shamil <shamilnk0458@gmail.com>
2025-02-28 11:47:52 +0530

    reverse a string using recursion

--- Recursion/basics.py
+++ Recursion/basics.py
@@ -18,9 +18,15 @@ def is_palidrome(strr):
         return False
     return is_palidrome(strr[1:-1])
 
-
+def reverse_string(strr):
+    # base case
+    if len(strr) <= 0 :
+        return strr
+    
+    return reverse_string(strr[1:]) + strr[0]
 
 
 print(find_factorail(6))
 print(find_sum_of_an_array([1,2,3,4,5],len([1,2,3,4,5])))
 print(is_palidrome('maaaaam'))
+print(reverse_string('abcdef'))
