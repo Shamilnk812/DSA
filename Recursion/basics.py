4a13d0cbd86390703609de5fdc4f13bf6ae49d4c
shamil <shamilnk0458@gmail.com>
2025-02-28 11:31:11 +0530

    Find sum of an array using recursion

--- Recursion/basics.py
+++ Recursion/basics.py
@@ -5,5 +5,11 @@ def find_factorail(num):
         return 1
     return num * find_factorail(num -1)
 
+def find_sum_of_an_array(arr,n):
+    if n <= 0:
+        return 0
+    return arr[n-1] + find_sum_of_an_array(arr,n-1)
+
 print(find_factorail(6))
+print(find_sum_of_an_array([1,2,3,4,5],len([1,2,3,4,5])))
 
