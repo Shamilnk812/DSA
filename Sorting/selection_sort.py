15366b8528309613d9dfeaecc1a77ae0e09aa614
shamil <shamilnk0458@gmail.com>
2025-03-08 21:06:36 +0530

    Refactored Selection Sort implementation

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,17 +1,21 @@
-def selection_sort(list):
 
-    for i in range(len(list)):
-        # Find the minimum value in the remaining unsorted portion of the list
-        min_val = min(list[i:])
-        
-        # Find the index of the minimum value in the unsorted portion
-        min_index = list.index(min_val, i)  # Use the start parameter to avoid searching the whole list
-        
-        # Swap the current element with the minimum value found
-        list[i], list[min_index] = list[min_index], list[i]
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_index = i  # Assume the first element is the minimum
+
+        # Find the index of the smallest element in the remaining array
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap the found minimum element with the first element
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Example usage:
 
 
-list = [64, 25, 12, 22, 11]
+list = [64, 12, 25, 12, 22, 11]
 selection_sort(list)
 print(list)
 
